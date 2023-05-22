b645ddf4930d6741a4af155a0b1d7dbbd010f93f
PC 2 <PC 2@DESKTOP-8ELQAGK>
2023-05-22 15:36:32 +0430

    Add project files.

---
+++ Arian.NModalReportViewer.TSProj/Pages/NModalReportViewer.ts
@@ -0,0 +1,23 @@
+﻿class NModalReportViewer extends Card {
+    constructor($mainPage: JQuery) {
+        super(' ');
+
+        this.$card.appendTo($mainPage);
+
+        var $row = $('<div class="row" />').appendTo(this.$body);
+        var $col4 = $('<div class="col col-4" />').append('&nbsp;').appendTo($row);
+        var $col8 = $('<div class="col col-8 text-left" />').appendTo($row);
+
+        var $actions = $('<div class="display-inline-block" />').appendTo($col4);
+
+        Button("NDR", ev => new ModalReportViewer("NDR","")).appendTo($actions.append(' '));
+
+
+
+    }
+}
+
+$(function () {
+    var btnNdr = new NModalReportViewer($("#mainPage"));
+
+})
